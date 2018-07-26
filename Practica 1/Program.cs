6c851b4ae01d1800210c27f77d15d414c2819c0a
Enmacomputer <40839110+Enmacomputer@users.noreply.github.com>
2018-07-26 17:30:15 -0400

    Actualizando

---
+++ Practica 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 50;
+			int b = 51;
+			Console.WriteLine("Valor de la variable a = 50");
+			Console.WriteLine("Valor de la variable b = 51");
+			Console.WriteLine("El resultado de multiplicar es igual a:" + (a * b));
+			Console.ReadKey();
+
+		}
+	}
+}
